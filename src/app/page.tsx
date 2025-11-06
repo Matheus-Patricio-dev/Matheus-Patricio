006e3bfdc68a3ca5f96d1f37438f978bfba7b68a
Matheus Patricio <patriciomtt@outlook.com>
2025-11-06 01:20:55 -0300

    novo emprego

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,22 @@ export default function Home() {
         <div className="mt-12">
           <h1 className="font-medium text-gray-900 mb-4 text-lg">Experience</h1>
           <ol className="relative border-s border-gray-200">
-            <li className="mb-10 ms-4">
+          <li className="mb-10 ms-4">
               <div className="absolute w-3 h-3 bg-green-500 rounded-full mt-1.5 -start-1.5 border "></div>
+              <div className="flex flex-row items-center gap-2">
+                <div className="text-md font-medium text-gray-900">
+                Software Engineer 
+                </div>
+                <div className="inline-block uppercase text-xs rounded-full px-1 py-0 border border-gray-500 text-gray-500">
+                  Oct 2025 - present
+                </div>
+              </div>
+              <div className="mb-4 text-sm font-normal text-gray-500">
+                Launcher Studio
+              </div>
+            </li>
+            <li className="mb-10 ms-4">
+              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border "></div>
               <div className="flex flex-row items-center gap-2">
                 <div className="text-md font-medium text-gray-900">
                 Software Developer 
